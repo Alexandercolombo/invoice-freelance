c0f4f43026aedb203faa81fec9e681bc42637183
Alex Colombo <alexandercolombo88@gmail.com>
2025-01-04 18:17:54 -0500

    Update Convex configuration for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@/convex': `${__dirname}/convex`
+      'convex/_generated/api': `${__dirname}/convex/_generated/api`,
+      'convex/_generated/dataModel': `${__dirname}/convex/_generated/dataModel`
     };
     return config;
   },

1e092a96ac3fcdc7c68ad8e832c3fc1a239b4ac1
Alex Colombo <alexandercolombo88@gmail.com>
2025-01-04 21:25:36 -0500

    Update TypeScript and Next.js configuration for Convex imports

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'convex/_generated/api': `${__dirname}/convex/_generated/api`,
-      'convex/_generated/dataModel': `${__dirname}/convex/_generated/dataModel`
-    };
-    return config;
-  },
   transpilePackages: ['convex']
 };
 

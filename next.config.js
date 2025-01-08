b5b633383ca95aa34bf7694ded2f9ac74f31f1b0
Alex Colombo <alexandercolombo88@gmail.com>
2025-01-08 17:15:21 -0600

    fix: update next config to handle convex files

--- next.config.js
+++ next.config.js
@@ -2,8 +2,16 @@
 const nextConfig = {
   transpilePackages: ['convex'],
   experimental: {
-    externalDir: true
-  }
+    externalDir: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Add the convex directory to be processed by webpack
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: /node_modules/,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 

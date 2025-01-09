c41f4eb9f5a9674ad924941c569c151904a5ecef
Alex Colombo <alexandercolombo88@gmail.com>
2025-01-08 20:43:05 -0600

    chore: add convex generated files

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,48 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as auth from "../auth.js";
+import type * as clients from "../clients.js";
+import type * as config from "../config.js";
+import type * as invoices from "../invoices.js";
+import type * as lib_validators from "../lib/validators.js";
+import type * as tasks from "../tasks.js";
+import type * as users from "../users.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
+  clients: typeof clients;
+  config: typeof config;
+  invoices: typeof invoices;
+  "lib/validators": typeof lib_validators;
+  tasks: typeof tasks;
+  users: typeof users;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;

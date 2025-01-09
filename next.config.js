3956250e8c7ea5eb51c3616cfc973cae57dd403a
Alex Colombo <alexandercolombo88@gmail.com>
2025-01-08 18:35:23 -0600

    fix: update build script to copy convex files

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   experimental: {
     externalDir: true,
   },
-  webpack: (config, { isServer }) => {
-    // Add the convex directory to module resolution
-    config.resolve.modules.push('convex');
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: /node_modules/,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig; 
